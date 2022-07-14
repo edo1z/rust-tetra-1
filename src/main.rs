89d3ca8d459c94c7a5f6b79e2bd26c58c2d5373a
web3ten0 <web3ten0@gmail.com>
2022-07-15 00:07:59 +0900

    画像を表示

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use tetra::graphics::{self, Color};
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
-struct GameState;
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+
+struct GameState {
+    paddle_texture: Texture,
+}
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
+        Ok(GameState { paddle_texture })
+    }
+}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        // Cornflower blue, as is tradition
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
         Ok(())
     }
 }
 
 fn main() -> tetra::Result {
-    ContextBuilder::new("Pong", 640, 480)
+    ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
         .build()?
-        .run(|_| Ok(GameState))
+        .run(GameState::new)
 }
